929cb3116986035c5271cf1c009abc709f92cec8
lisbeth estevez <estevez34545@gmail.com>
2020-07-30 16:15:46 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,9 @@
-def unsafe?(speed)
-  require "pry"
- if speed < 40
-    "true"
-    binding.pry
-    elsif speed > 60
-    "true"
-  else 
-    "false"
-  end 
+def not_safe?(speed)
+
+  if speed < 40 || speed > 60 ? true : false
+
+  end
+end
 end
 
 
